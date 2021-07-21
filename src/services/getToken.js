722948dc51e8c8fae5189469ac95735a14a3f9b2
NUSmhk <e0550349@u.nus.edu>
2021-07-21 23:16:22 +0800

    Added Mongo backend

--- src/services/getToken.js
+++ src/services/getToken.js
@@ -1,11 +1,9 @@
-import firebase from "firebase";
+  import firebase from "firebase";
 
-// Best way? Check firebase docs?
 async function getToken() {
     const user = firebase.auth().currentUser;
     const token = user && (await user.getIdToken());
 
-    // Bearer - refers to token-based authentication
     const payloadHeader = {
         headers: {
             'Content-Type': 'application/json',
